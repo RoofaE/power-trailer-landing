79070ea78e7f6dba34ab2e07f90b99ca2758d3cc
Roofa <roofa.etagiuri1@gmail.com>
2026-07-18 22:35:30 -0600

    fix: contact submit form

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, error: 'Method not allowed' });
   }
